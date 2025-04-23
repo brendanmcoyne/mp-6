df0401c6d2af6d45e790dc40f5d1b1612eb7cade
brendanmcoyne <bmcoyne@bu.edu>
2025-04-23 19:30:27 -0400

    First push

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between bg-white items-center h-16">
+            <h2 className="text-3xl font-semibold p-4 text-black">CS 391 OAuth</h2>
+        </header>
+    );
+}
