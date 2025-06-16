9a4053767097285a687a45a75f56b9fd4d0d8979
brendanmcoyne <bmcoyne@bu.edu>
2025-06-16 00:46:32 -0400

    Attempt for github

--- app/sign-in/github/route.ts
+++ app/sign-in/github/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     const clientId = process.env.GITHUB_CLIENT_ID!;
-    const redirectUri = encodeURIComponent(process.env.OAUTH_REDIRECT_GITHUB!);
+    const redirectUri = 'https://mp-6-brown.vercel.app/api/auth/callback?provider=github';
     const scope = 'user';
-
     const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+
     return NextResponse.redirect(authUrl);
 }
