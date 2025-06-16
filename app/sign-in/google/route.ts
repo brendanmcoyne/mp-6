4c701068d85aa52858242240b3d2b05bb26fa6f0
brendanmcoyne <bmcoyne@bu.edu>
2025-06-16 00:44:13 -0400

    google retest

--- app/sign-in/google/route.ts
+++ app/sign-in/google/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const clientId = process.env.GOOGLE_CLIENT_ID!;
-    const redirectUri = encodeURIComponent(process.env.OAUTH_REDIRECT_GOOGLE!);
-
-    const scope = encodeURIComponent('openid email profile');
-
+    const clientId = process.env.CLIENT_ID!;
+    const redirectUri = 'https://mp-6-brown.vercel.app/api/auth/callback';
+    const scope = 'openid email profile';
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&access_type=offline&prompt=consent`;
 
     return NextResponse.redirect(authUrl);
